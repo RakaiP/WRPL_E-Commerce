001c263b7b1ad0e721ee82438dc1dfac3173939a
Abhinawap <149473741+Abhinawap@users.noreply.github.com>
2025-06-26 15:42:32 +0700

    Create jest.config.js

---
+++ store/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  verbose: true
+};
